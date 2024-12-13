2148e2d68cd569f0988de44d44290fc023c0efff
StonedBlocks <linusmine2204@gmail.com>
2024-12-13 19:21:35 +0700

    Initial commit

---
+++ src/main/java/net/thenameislinus/dyomwood/DyomWoodClient.java
@@ -0,0 +1,10 @@
+package net.thenameislinus.dyomwood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DyomWoodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/thenameislinus/dyomwood/DyomWoodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thenameislinus.dyomwood;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DyomWoodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
